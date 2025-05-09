a7ca43c253e00b6fbfab6dc007043aac172f99a4
Camila R Lauriano <ariel.r.lauriano@gmail.com>
2025-05-09 17:20:54 -0300

    Create ComparacaoNumerica.py

---
+++ logica_programacao/Semestre01/Aula04/ComparacaoNumerica.py
@@ -0,0 +1,4 @@
+a = float(input("Digite um número: "))
+b = float(input("Digite um número: "))
+
+print(f"{a} = a\n {b} = b\n a == b is {a == b}\n a == (b/2) is {a == (b/2)}\n a != b is {a!=b}\n a > b is {a>b}\n a < b is {a<b}")
